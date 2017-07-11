73f5375fe1abc3db8934ecc00f34f12fcc19f0c9
Godsave Joseph <gsavejoseph@gmail.com>
2017-07-12 00:44:08 +0100

    Add files via upload
    Getting better but still a long way to go!

---
+++ Revendra/js/revendra.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    });
+
+    // Initialize and Configure Scroll Reveal Animation
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+    sr.reveal('.sr-cars', {
+        duration: 1000,
+        distance: '0px'
+    }, 200);
+});
