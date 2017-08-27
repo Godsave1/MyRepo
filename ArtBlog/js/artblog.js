cc316eac575df5d3b1ce04aef8b3690c3260b0c7
Godsave Joseph <gsavejoseph@gmail.com>
2017-08-28 00:12:01 +0100

    Add files via upload
    Added Artblog!

---
+++ ArtBlog/js/artblog.js
@@ -0,0 +1,17 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    });
+
+
+})(jQuery); // End of use strict
